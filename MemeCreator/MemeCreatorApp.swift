3a67edca8d280c70e8526d02618ea10b9327e813
Christian Manzaraz <magnusmanz@gmail.com>
2024-05-15 18:59:14 -0300

    Initial Commit

---
+++ MemeCreator/MemeCreatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemeCreatorApp.swift
+//  MemeCreator
+//
+//  Created by Christian Manzaraz on 15/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemeCreatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
